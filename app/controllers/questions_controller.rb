d6432c18f62a0fc6f4ef3c341bb0601a068dc599
Tristan Schmitt <trschmitt50@live.com>
2017-09-07 09:44:40 -0400

    Bugs in Master Branch fixed Ready for day 2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,6 +24,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def edit
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,12 +1,12 @@
 class Question < ApplicationRecord
-  has_many :answers
-  belongs_to :user
+  # has_many :answers
+  # belongs_to :user
 
   def markdown
-   md = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+    md = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
       no_intra_emphasis: true,
       fenced_code_blocks: true,
       disable_indented_code_blocks: true)
-   md.render(self.body).html_safe
- end
+    md.render(self.body).html_safe
+  end
 end
