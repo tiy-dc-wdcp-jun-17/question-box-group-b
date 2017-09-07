b66091e5cb2de20279725709319605a6b5bd2a7d
Tristan Schmitt <trschmitt50@live.com>
2017-09-07 11:58:15 -0400

    Sessions Controller and views created. User authentication logic created. Logged in users redirected correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   # has_many :questions
 
-  # validates :username, :password, presence: true
+  has_secure_password
+  validates :email,
+    uniqueness: true,
+    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+
+  private
+
+  def downcase_email
+    email.downcase if email
+  end
+
 end
