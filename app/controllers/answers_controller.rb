75ab81e466490bd49a765916a8434992d88a4c2f
Leonard <leonard.horne@gmail.com>
2017-09-07 16:37:24 -0400

    Store new answer.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,11 @@
 class AnswersController < ApplicationController
   def new
+    @question = Question.find(params[:question_id])
     @answer = Answer.new
   end
 
   def create
-    @answer = Answer.new(answer_params)
+    @answer = Answer.new(params[:question_id])
     respond_to do |format|
       if @answer.save
         # this may not be correct, but we want to redirect to the question view that that answer is responding to.
