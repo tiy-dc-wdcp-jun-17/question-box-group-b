12dcfab0f85784fea2a0430525d2dd46b6b2c4f5
Leonard <leonard.horne@gmail.com>
2017-09-08 10:28:54 -0400

    Ensure redirect to login when not already logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_user
+
+  def authenticate_user!
+    redirect_to new_users_path unless current_user
+  end
 end
