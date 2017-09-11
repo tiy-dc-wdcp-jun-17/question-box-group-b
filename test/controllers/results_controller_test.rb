195022deff0002d7d776e9ed795a5ebcde721bcf
Tristan Schmitt <trschmitt50@live.com>
2017-09-11 11:48:05 -0400

    NavBar Search box and input created

---
+++ test/controllers/results_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ResultsControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get index" do
+    get results_index_url
+    assert_response :success
+  end
+
+end
