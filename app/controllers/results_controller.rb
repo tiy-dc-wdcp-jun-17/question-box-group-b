fe3984a7d04628625a62a8a1bed4f499a0fa16ef
Leonard <leonard.horne@gmail.com>
2017-09-11 15:15:14 -0400

    Add pagination of results.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,5 @@
 class ResultsController < ApplicationController
   def index
-    @pg_search_documents = PgSearch.multisearch(params[:q])
+    @pg_search_documents = PgSearch.multisearch(params[:q]).page(params[:page]).per(10)
   end
 end
